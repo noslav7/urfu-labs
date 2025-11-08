2d1db0830ced9686b5161bd9c33fdd7a9ba20ea0
noslav7 <noslav7@gmail.com>
2025-11-08 20:02:10 +0500

    timus task 05

---
+++ src/main/java/ru/urfu/timus/task05_2012/Task05_2012_AboutGrisha.java
@@ -0,0 +1,29 @@
+package ru.urfu.timus.task05_2012;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Task05_2012_AboutGrisha {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int numberOfTasks = input.nextInt();
+
+        int difference = 12 - numberOfTasks;
+
+        int time = difference * 45;
+
+        String result = time <= 240 ? "YES" : "NO";
+
+//        if (time <= 240) {
+//            System.out.println("YES");
+//        } else {
+//            System.out.println("NO");
+//        }
+
+        System.out.println(result);
+        out.flush();
+    }
+}
