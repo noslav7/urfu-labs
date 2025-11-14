05c815a647ecdbbcc808d6a0c51fb43a1a9aaa6e
noslav7 <noslav7@gmail.com>
2025-11-14 19:40:52 +0500

    timus task 7

---
+++ src/main/java/ru/urfu/timus/task07_2001/Task07_2001_MathematiciansBerries.java
@@ -0,0 +1,25 @@
+package ru.urfu.timus.task07_2001;
+
+import java.util.Scanner;
+
+public class Task07_2001_MathematiciansBerries {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a1 =  input.nextInt();
+        int b1 =  input.nextInt();
+        int a2 =  input.nextInt();
+        int b2 =  input.nextInt();
+        int a3 =  input.nextInt();
+        int b3 =  input.nextInt();
+
+        int berries2 = b1 - b2;
+        int berries1 = b3 - berries2 - b2;
+
+        System.out.println(berries1 + " " + berries2);
+
+
+        input.close();
+    }
+}
