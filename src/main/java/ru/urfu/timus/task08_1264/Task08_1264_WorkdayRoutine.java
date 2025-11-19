7678beec183ed2c7a564fb01a61087332e3df152
noslav7 <noslav7@gmail.com>
2025-11-19 08:59:05 +0500

    timus task 8

---
+++ src/main/java/ru/urfu/timus/task08_1264/Task08_1264_WorkdayRoutine.java
@@ -0,0 +1,19 @@
+package ru.urfu.timus.task08_1264;
+
+import java.util.Scanner;
+
+public class Task08_1264_WorkdayRoutine {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int m = input.nextInt();
+
+        int numberOfM = m + 1;
+
+        int result = n * numberOfM;
+
+        System.out.println(result);
+    }
+}
