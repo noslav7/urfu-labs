936b13a293cac672dcb8cf90dd7d9f9cbfe02b56
noslav7 <noslav7@gmail.com>
2025-11-27 20:01:44 +0500

    timus tasks 9, 10

---
+++ src/main/java/ru/urfu/timus/task10_1820/Task10_1787_UralChopSteaks.java
@@ -0,0 +1,16 @@
+package ru.urfu.timus.task10_1820;
+
+import java.util.Scanner;
+
+public class Task10_1787_UralChopSteaks {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int k = in.nextInt();
+
+        int time = (2 * n + k - 1) / k;
+        System.out.println(time);
+    }
+}
